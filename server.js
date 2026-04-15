42f1a9e4b0d474d9de3547dc83039c4b665ec228
Zeeshanakbar444 <you@example.com>
2026-04-14 19:56:10 -0700

    changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'https://society-frontend.vercel.app/',
     credentials: true
 }));
 app.use(express.json());
